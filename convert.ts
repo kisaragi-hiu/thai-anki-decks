57d3aac84dd5c4b5d35a83e0c4694e36376ff197
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-05-05 01:14:59 +0900

    Make it possible for me to write a different schema

--- convert.ts
+++ convert.ts
@@ -13,39 +13,15 @@ process.on("uncaughtException", (err) => {
   process.exitCode = 1;
 });
 
-const entry = type({
-  /** Indo-Arabic "123" digits */
-  arabic: "string",
-  /** Thai digits */
-  thai: "string",
-  /** Thai name of the number plus IPA */
-  pn: "string",
-}).array();
-
-async function main() {
-  const parsedArgs = parseArgs({
-    args: process.argv.slice(2),
-    options: {
-      help: { type: "boolean", short: "h" },
-      input: { type: "string", short: "i" },
-      output: { type: "string", short: "o" },
-    },
-  });
-
-  if (parsedArgs.values.help) {
-    console.log(`<convert.ts> -i <input.yaml> -o <output.apkg>
-
-Options:
-  --input | -i: Specify input YAML file (required)
-  --output | -o: Specify output .apkg file (required)
-  --help | -h: Show help (this message)`);
-    process.exit(0);
-  }
-  const { input: inputPath, output: outputPath } = parsedArgs.values;
-  if (!(typeof inputPath === "string" && typeof outputPath === "string")) {
-    console.log(`Input and output paths must both be specified`);
-    process.exit(1);
-  }
+function convertNumbersDeck(inputPath: string, outputPath: string) {
+  const entry = type({
+    /** Indo-Arabic "123" digits */
+    arabic: "string",
+    /** Thai digits */
+    thai: "string",
+    /** Thai name of the number plus IPA */
+    pn: "string",
+  }).array();
 
   const data = entry(load(readFileSync(inputPath, { encoding: "utf-8" })));
   if (data instanceof type.errors) {
@@ -53,7 +29,6 @@ Options:
   }
 
   const apkg = new AnkiExport("Thai numbers");
-
   for (const entry of data) {
     apkg.addCard(entry.arabic, entry.thai + "\n\n" + entry.pn);
     apkg.addCard(entry.thai, entry.arabic + "\n\n" + entry.pn);
@@ -66,6 +41,47 @@ Options:
       `New deck based on ${inputPath} has been written to ${outputPath}.`,
     );
   });
+}
+
+async function main() {
+  const parsedArgs = parseArgs({
+    args: process.argv.slice(2),
+    options: {
+      help: { type: "boolean", short: "h" },
+      deck: { type: "string", short: "d" },
+      input: { type: "string", short: "i" },
+      output: { type: "string", short: "o" },
+    },
+  });
+
+  if (parsedArgs.values.help) {
+    console.log(`<convert.ts> -i <input.yaml> -o <output.apkg>
+
+Options:
+  --deck: Which deck to generate. Possible choices:
+    "numbers": convert using the numbers deck format
+    "vowels": convert using the vowels deck format
+  --input | -i: Specify input YAML file (required)
+  --output | -o: Specify output .apkg file (required)
+  --help | -h: Show help (this message)`);
+    process.exit(0);
+  }
+  const { input: inputPath, output: outputPath, deck } = parsedArgs.values;
+  if (!(typeof inputPath === "string" && typeof outputPath === "string")) {
+    console.log(`Input and output paths must both be specified`);
+    process.exit(1);
+  }
+  const deckOptionSchema = type.enumerated("numbers", "vowels");
+  const parsedDeck = deckOptionSchema(deck);
+  if (parsedDeck instanceof type.errors) {
+    console.log(
+      `--deck must be specified and must be a valid option (see --help)`,
+    );
+    process.exit(1);
+  }
+  if (parsedDeck === "numbers") {
+    convertNumbersDeck(inputPath, outputPath);
+  }
 }
 
 main();
